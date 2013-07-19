b297c7539fcfef78c39bffad9ba7ea090d9b25e9
Dan Dorenfeld <dan.dorenfeld@gmail.com>
2013-07-19 09:46:40 -0600

    Initial commit

---
+++ DateTimeCountdown/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DateTimeCountdown::Application.config.secret_token = '36aa4be8ff1e165d5092ec6afe67161b5eb66003b568e5cec6bcc52e253e60ed5c79881e4fff27cb194c024832b68bcf02c87faebe8bb5135aa09ca1450533fe'
